859c95546c98a5f0a7f8644ea914689711158733
@CaiqueBraga <caiquebsousa@outlook.com>
2023-10-09 09:14:23 -0300

    alteração-layout-pag-inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,164 +1,51 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    </head>
-    <style>
-    /* Reset de estilos */
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-
-/* Estilo global */
-body {
-    font-family: Arial, sans-serif;
-    background-color: #f0f0f0;
-    color: #333;
-    line-height: 1.6;
-}
-
-/* Estilos do cabeçalho */
-header {
-    background-color: #333;
-    color: #fff;
-    padding: 20px;
-    text-align: center;
-}
-
-header h1 {
-    font-size: 2rem;
-}
-
-nav ul {
-    list-style: none;
-}
-
-nav ul li {
-    display: inline;
-    margin-right: 20px;
-}
-
-nav a {
-    text-decoration: none;
-    color: #fff;
-    font-weight: bold;
-}
-
-/* Estilos da seção de destaque */
-.hero {
-    text-align: center;
-    padding: 50px 0;
-}
-
-.hero h2 {
-    font-size: 2.5rem;
-    margin-bottom: 20px;
-}
-
-.hero p {
-    font-size: 1.2rem;
-    margin-bottom: 30px;
-}
-
-.cta-button {
-    display: inline-block;
-    padding: 10px 20px;
-    background-color: #333;
-    color: #fff;
-    text-decoration: none;
-    font-weight: bold;
-    border-radius: 5px;
-    transition: background-color 0.3s;
-}
-
-.cta-button:hover {
-    background-color: #555;
-}
-
-/* Estilos dos cursos em destaque */
-.featured-courses {
-    text-align: center;
-    padding: 40px 0;
-}
-
-.featured-courses h2 {
-    font-size: 2rem;
-    margin-bottom: 30px;
-}
-
-.course {
-    display: inline-block;
-    margin: 0 20px;
-    text-align: left;
-}
-
-.course img {
-    max-width: 100%;
-    height: auto;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-}
-
-.course h3 {
-    font-size: 1.5rem;
-    margin-top: 10px;
-}
-
-.course p {
-    margin-top: 10px;
-}
-
-/* Estilos do rodapé */
-footer {
-    background-color: #333;
-    color: #fff;
-    text-align: center;
-    padding: 10px 0;
-}
-/* Estilização para todas as imagens */
-/* Estilização para todas as imagens */
-img {
-    width: 200px; /* Defina a largura desejada */
-    height: 150px; /* Defina a altura desejada */
-    object-fit: cover; /* Define como a imagem deve se ajustar ao contêiner (pode usar 'cover', 'contain', etc.) */
-}
-
-
-
-    </style>
-    <body>
-        <x-navbar></x-navbar>
-        <section class="hero">
-            <h2>Bem-vindo ao nosso sistema de aprendizado online</h2>
-            <p>Transforme seu conhecimento e alcance seus objetivos de aprendizado conosco.</p>
-            <a href="#" class="cta-button">Comece Agora</a>
-        </section>
-
-        <section class="featured-courses">
-            <h2>Cursos em Destaque</h2>
-            <div class="course">
-                <img src="img/logicaprog.png" alt="Curso 1">
-                <h3>Logica de programção</h3>
-                <p>Descrição do Curso 1.</p>
-                <a href="#" class="cta-button">Saiba Mais</a>
-            </div>
-            <div class="course">
-                <img src="img/php.jpg" alt="Curso 2">
-                <h3>Php com PDO</h3>
-                <p>Descrição do Curso 2.</p>
-                <a href="#" class="cta-button">Saiba Mais</a>
-            </div>
-            <!-- Adicione mais cursos aqui -->
-        </section>
-    </body>
+<html lang="en">
+<head>
+  <title>Bootstrap Card</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+  <x-navbar></x-navbar>
+  <div class="container mt-5 text-center">
+    <h1>Confira alguns dos nossos cursos</h1>
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="img/php.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="img/logicaprog.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="img/js.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
 </html>
-
