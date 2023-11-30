6af5c18cfe47fec91d5d775e7f51aa9e4f9066d8
Caiquebraga <caiquebsousa@outlook.com>
2023-11-30 05:33:19 -0300

    [refs: 0.19 - FEAT - metodos_service_user]

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Repositories\UserRepository;
 use Facade\FlareClient\Http\Response;
 use Illuminate\Http\Response as HttpResponse;
+use Illuminate\Support\Facades\Validator;
 
 class Userservice 
 {
@@ -24,5 +25,48 @@ class Userservice
            'status' => Response::HTTP_OK
         ];
     }
+
+    public function createUser(array $userData)
+    {
+        $rules = [
+            'nome' => 'required|string|min:2|max:255',
+            'email' => 'required|email|unique:users',
+            'senha' => 'required|string|min:8|max:255|confirmed',
+        ];
+
+        $messages = [
+            'nome.required' => 'O campo nome é obrigatório.',
+            'email.required' => 'O campo email é obrigatório.',
+            'senha' => 'O campo senha deve ter no minino 8 digitos.',
+        ];
+
+          // Execute a validação
+          $validator = Validator::make($userData, $rules, $messages);
+
+          // Verifique se a validação falhou
+          if ($validator->fails()) {
+              return redirect('register')
+                          ->withErrors($validator)
+                          ->withInput();
+          }
+        
+        $user = $this->userRepository->create($userData);
+        return $user;
+    }
+
+
+    public function updateUser(array $userData)
+    {
+        $updatedUser = $this->userRepository->updateUser($userData);
+
+        return $updatedUser;
+    }
+
+    
+    public function deleteUser()
+    {
+       $deleteUser = $this->UserRepository->deleteUser();     
+    }
+
 
 }
