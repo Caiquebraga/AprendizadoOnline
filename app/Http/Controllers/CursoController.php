0c0cf3f67a450c745cef0edbae518f7e16a14b8a
Caiquebraga <caiquebsousa@outlook.com>
2024-02-26 05:15:02 -0300

    [Refs: 0.0.36 - FEAT - Criação do metodo CriaCurso na controller CursoController]

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -15,4 +15,22 @@ class CursoController extends Controller
     {
         $this->cursoService = $cursoService;
     }
+
+    public function criaCurso (Request $request)
+    {
+            try{
+            $dados = $request->all();
+
+            $inscricao = $this->cursoService->criarCurso($dados);
+
+            return response()->json(['message' => 'Curso Criado com Sucessso', 'curso'=> $inscricao], 201);
+
+            } catch (\InvalidArgumentException $e){
+            return response()->json(['error' => $e->getMessage()]. 400);
+            }  catch (\RuntimeException $e) {
+                return response()->json(['error' => $e->getMessage(), 409]);
+            } catch (\Exception $e){
+                return response()->json(['error' => 'Ocorreu um erro ao criar o curso'], 500);
+            }
+    }
 }
